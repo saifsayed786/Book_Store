55a54769b5a4147496f2cf27995011c26914e609
saifuddinsayed786 <saifuddin@nimapinfotech.com>
2021-11-12 23:40:17 +0530

    Added New Files

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Book_Store
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"Book_Store/models"
+
+	"github.com/gin-gonic/gin"
+
+	"Book_Store/controllers"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Connect to database
+	models.ConnectDatabase()
+
+	// Routes
+	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	// Run the server
+	r.Run()
+}
